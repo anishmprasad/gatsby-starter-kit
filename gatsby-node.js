9e8615cedf702fa8b0a9a7d5388f0474730b8ee5
Anish.M.Prasad <anish.m.prasad@gmail.com>
2020-01-23 00:47:31 +0530

    build update

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,40 @@
  */
 
 // You can delete this file if you're not using it
+
+const path = require(`path`);
+const axios = require("axios");
+// Log out information after a build is done
+exports.onPostBuild = ({ reporter }) => {
+  reporter.info(`Your Gatsby site has been built!`);
+};
+// Create blog pages dynamically
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+  const result = await axios.get("https://api.myjson.com/bins/cw686");
+  console.log(result);
+  const IndexPage = path.resolve(`src/pages/index.js`);
+  console.log({ IndexPage });
+  //   const result = await graphql(`
+  //     query MyQuery {
+  //       allSitePlugin {
+  //         edges {
+  //           node {
+  //             id
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `);
+  //   console.dir(result);
+  // result.data.allSitePlugin.edges.forEach(edge => {
+  //   console.log(edge);
+  createPage({
+    path: `/`,
+    component: IndexPage,
+    context: {
+      data: "result"
+    }
+  });
+  // });
+};
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,30 @@
  */
 
 // You can delete this file if you're not using it
+
+const React = require("react");
+const Layout = require("./src/components/layout");
+// Adds a class name to the body element
+exports.onRenderBody = ({ setBodyAttributes }, pluginOptions) => {
+  setBodyAttributes({
+    className: "my-body-class"
+  });
+};
+// Wraps every page in a component
+exports.wrapPageElement = ({ element, props }) => {
+  return <Layout {...props}>{element}</Layout>;
+};
+
+// Move Typography.js styles to the top of the head section so they're loaded first.
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents();
+  headComponents.sort((x, y) => {
+    if (x.key === "TypographyStyle") {
+      return -1;
+    } else if (y.key === "TypographyStyle") {
+      return 1;
+    }
+    return 0;
+  });
+  replaceHeadComponents(headComponents);
+};
